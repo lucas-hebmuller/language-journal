d36347f41162461daae9ad9f056ef6d76099816d
Lucas Hebmuller <lucas.hebmuller@gmail.com>
2026-03-17 07:36:53 -0400

    initial setup

---
+++ backend/src/main/java/com/languagejournal/languagejournal/LanguageJournalApplication.java
@@ -0,0 +1,13 @@
+package com.languagejournal.languagejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageJournalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/languagejournal/languagejournal/LanguageJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.languagejournal.languagejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
